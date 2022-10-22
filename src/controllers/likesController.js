64c3fd583aacbf875d509e93c03e9d99a6e810e9
emir4ik-code <emirahmetov@gmail.com>
2022-10-22 00:50:00 -0400

    likesController

---
+++ src/controllers/likesController.js
@@ -0,0 +1,45 @@
+const pool = require("../config/dbConfig");
+
+class LikesController {
+  static async getLikesOfPost(req, res) {
+    try {
+      const { postId } = req.params;
+      const likes = await pool.query(
+        "SELECT COUNT(*) FROM likes WHERE post_id = $1",
+        [postId]
+      );
+      return res.status(200).json(likes.rows[0]);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json("Server error");
+    }
+  }
+  static async likePost(req, res) {
+    try {
+      const { postId } = req.params;
+      const newLike = await pool.query(
+        "INSERT INTO likes (post_id,  user_id) VALUES($1, $2) RETURNING *",
+        [postId, req.user]
+      );
+      return res.status(200).json(newLike.rows[0]);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json("Server error");
+    }
+  }
+  static async deleteLikeFromPost(req, res) {
+    try {
+      const { postId } = req.params;
+      const deletedLike = await pool.query(
+        "DELETE FROM likes WHERE user_id = $1 AND post_id = $2 RETURNING *",
+        [postId, req.user]
+      );
+      return res.status(200).json(deletedLike.rows[0]);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json("Server error");
+    }
+  }
+}
+
+module.exports = LikesController;
