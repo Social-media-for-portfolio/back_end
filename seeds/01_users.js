54b4dca7d19450da1eb6cebab8bdaff2f60aaa23
emir4ik-code <emirahmetov@gmail.com>
2022-10-25 23:00:33 -0400

    avatars changed in seeds

--- seeds/01_users.js
+++ seeds/01_users.js
@@ -12,21 +12,24 @@ exports.seed = async function (knex) {
       last_name: "Akhmetov",
       password: "123",
       email: "emir@gmail.com",
-      avatar_url: avatar,
+      avatar_url:
+      "https://i.imgur.com/mnsUB79.jpeg"
     },
     {
       first_name: "Ray",
       last_name: "Lu",
       password: "123",
       email: "ray@gmail.com",
-      avatar_url: avatar,
+      avatar_url:
+        "https://media-exp1.licdn.com/dms/image/C4E03AQEcFcYPvmJKWw/profile-displayphoto-shrink_800_800/0/1637020996535?e=1672272000&v=beta&t=_ZvCb9-bPANycgGwjbYKjCL7X0xmQVLbeDRf-_kSO0w",
     },
     {
       first_name: "Matt",
       last_name: "Tan",
       password: "123",
       email: "matt@gmail.com",
-      avatar_url: avatar,
+      avatar_url:
+        "https://media-exp1.licdn.com/dms/image/D4D35AQHAPv3WKZj42w/profile-framedphoto-shrink_800_800/0/1637701691681?e=1667358000&v=beta&t=1EEapWnsmXQ7JoYgcpmy4sqlip8CDDvGBRL3NSDXULs",
     },
   ]);
 };
