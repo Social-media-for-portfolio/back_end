69c51a6836c1518966d40cc338338176aa37c1b6
emir4ik-code <emirahmetov@gmail.com>
2022-10-28 13:37:42 -0400

    seeds for friend requests

---
+++ seeds/friends.js
@@ -0,0 +1,13 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.seed = async function (knex) {
+  // Deletes ALL existing entries
+  await knex("friend_requests").del();
+  await knex("friend_requests").insert([
+    { sender_id: "4", receiver_id: "1", is_accepted: true },
+    { sender_id: "4", receiver_id: "2", is_accepted: true },
+    { sender_id: "4", receiver_id: "3", is_accepted: true },
+  ]);
+};
